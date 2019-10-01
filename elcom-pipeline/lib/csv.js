30e2f5bddd9bf7295d0a207e142dd1bcec3cde9b
Nicky <Nicky@192.168.0.214>
2019-10-01 13:26:23 +0200

    Working pipeline Elcom.

--- elcom-pipeline/lib/csv.js
+++ elcom-pipeline/lib/csv.js
@@ -1,10 +1,8 @@
 const { createReadStream } = require('fs')
 const path = require('path')
 
-function openFromCsvw (csvwPath) {
-  const csvw = require(`../${csvwPath}`)
-
-  return createReadStream(path.resolve(__dirname, csvw.url)) //create stream of the path
+function openFromCsvw (csvwPath, sourceFileUrl) {
+  return createReadStream(path.resolve(__dirname, '../source/'+sourceFileUrl)) //create stream of the path
 }
 
 module.exports = {
